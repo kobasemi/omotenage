cfa27905cddc209c09a048b799b6c2a11608e1a5
asakasa <asakasa@me.com>
2014-10-18 01:33:27 +0900

    Update index.js 前回と今回の翻訳先言語が同一の場合，翻訳しないといった処理を追加

--- webrtc/js/index.js
+++ webrtc/js/index.js
@@ -1,4 +1,7 @@
 
+// whether or not passed #home
+var passhome_flg = false;
+
 $(document).on('pageinit', '#home', function(e, data){
     // Create supported country listview
     $.getJSON("SupportCountry.json", function(data){
@@ -12,21 +15,38 @@ $(document).on('pageinit', '#home', function(e, data){
             $("#pick").listview().listview("refresh");
         });
     });
-});
 
-$(document).on('pageinit', '#nav', function(e, data){
     // Initialize page for english
     update_navpage('us', 'en');
+    passhome_flg = true;
 });
 
+$(document).on('pagebeforecreate', '#nav', function(e, data){
+    // -> User reload in this page
+    if(!passhome_flg){
+        // If not passed #home, move #home
+        $.mobile.changePage("#home");
+    }
+});
+
+// Original Text Language
+var pre_lc = "en";
 // Move #nav page
 function move_navpage(){
+
     // Get selected country code
     var cc = $(this).attr('id');
     // Get language code of selected country
     var lc = $('input[name="langcode"]', this).val();
 
-    update_navpage(cc, lc);
+    if(lc !== pre_lc){
+        // If the translation TARGET doesn't equal to the previous TARGET,
+        // translate to the TARGET
+        update_navpage(cc, lc);
+        // Previous country language is updated
+        pre_lc = lc;
+    }
+
     $.mobile.changePage("#nav");
 }
 
