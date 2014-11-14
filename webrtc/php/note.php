838e5178c4f811e813c55960712022f01087baf1
asakasa <asakasa@me.com>
2014-11-15 04:33:15 +0900

    Update Translate Program in index.html 不必要なPHPファイルを削除し，一つのファイルに統合

--- webrtc/php/note.php
+++ webrtc/php/note.php
@@ -1,9 +1,19 @@
 <?php
 
-// Read translate function
-require "translate.php";
-// Read Original Contents
-require "orgtxt.php";
+// Original Body Text
+$orgtxt = array(
+    // line 1
+    'l1' => 'In order to do the best "OMOTENASHI", expert guide will show you around the street of Tokyo.',
+    // line 2
+    'l2' => 'Pushing the button below, you just call a guide.',
+    // line 3
+    'l3' => 'NOTE: Next Page needs to allow you to get Camera and Mic, GPS.'
+);
+// Main Contents
+$contents = array(
+    "header" => 'Welcome to Japan',
+    "body" => $orgtxt
+);
 
 // Original Text Language: English
 $from = 'en';
@@ -20,6 +30,8 @@ if($from === $to){
     exit();
 }
 
+// Read translate function
+require "translate.php";
 // BEGIN Translation
 $contents['header'] = translate($contents['header'], $from, $to);
 foreach($contents['body'] as $key => $value){
